457f907b2d3c3c13fb8997395c8c01b51a80eebf
namtranhoai <namtranhoai@gmail.com>
2020-04-24 08:45:31 -0600

    save state to localStorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -15,9 +15,31 @@ const reducers = combineReducers({
   UI: uiReducer,
 });
 
+function saveToLocalStorage(state){
+    try{
+        const serializedState = JSON.stringify(state)
+        localStorage.setItem('state', serializedState)
+    }catch(e){
+        console.log(e);
+    }
+}
+function loadFromLocalStorage(){
+    try{
+        const serializedState = localStorage.getItem('state')
+        if(serializedState === null) return undefined
+        return JSON.parse(serializedState)
+    }catch(e){
+        console.log(e)
+        return undefined
+    }
+}
+const persistedState = loadFromLocalStorage()
 const composeEnhancers = composeWithDevTools({trace: true, traceLimit: 25 });
 const store = createStore(
   reducers,
+  persistedState,
   composeEnhancers(applyMiddleware(...middleware))
 );
+store.subscribe(()=> saveToLocalStorage(store.getState()))
+
 export default store;
